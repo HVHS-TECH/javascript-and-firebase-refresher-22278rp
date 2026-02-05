08f223b343bcb754443ccda36d6e96ec96fdbb2c
22278rp <22278rp@hvhs.school.nz>
2026-02-05 13:29:08 +1300

    fix2

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-
-
+function getElementbyId() {
+    
+}
 function start() {
     spaceForJavaScriptOutput.innerHTML
+    var messageSpace = getElementbyId("HELLO WORLD");
     messageSpace.innerHTML = "You've connected to the JavaScript!";
 }
