76a6dfb08ed9e9cee0f5d4593c5a66b9b407e137
22278rp <22278rp@hvhs.school.nz>
2026-02-08 11:28:51 +1300

    Version 5 Added button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getElementbyId() {
     
 }
 function start() {
-    spaceForJavaScriptOutput.innerHTML
-    var messageSpace = getElementbyId("HELLO WORLD");
     messageSpace.innerHTML = "You've connected to the JavaScript!";
+    buttonPressed.innerHTML = "You pressed the button!";
+    console.log ("hello");
 }
